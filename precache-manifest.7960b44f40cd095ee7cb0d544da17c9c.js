86b7c5ba3c2ec9ae823a0ea434a9b43d01c82f54
Nataliia Korneiko <korneyko_ns@ukr.net>
2019-11-19 17:56:05 +0200

    Updates

---
+++ precache-manifest.7960b44f40cd095ee7cb0d544da17c9c.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27b3062a8d615d25fa8ba7c41ecd3f93",
+    "url": "/goit-react-hw-02-reader/index.html"
+  },
+  {
+    "revision": "69314f15ebccfc7d7666",
+    "url": "/goit-react-hw-02-reader/static/js/2.e839df6f.chunk.js"
+  },
+  {
+    "revision": "45e60c4076a5b8b78038",
+    "url": "/goit-react-hw-02-reader/static/js/main.2c7b78ac.chunk.js"
+  },
+  {
+    "revision": "1f9652783983b1574946",
+    "url": "/goit-react-hw-02-reader/static/js/runtime-main.45a77436.js"
+  }
+]);
